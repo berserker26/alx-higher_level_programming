5b43437421c5845fd45f949716454422067eb7fc
Yoseph <yosephwondimu26@gmail.com>
2023-12-01 00:50:02 +0300

    4. Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     n = dir(hidden_4)
     lim_t = len(n)
     for i in range(0, lim_t):
-        if file[i][0:2] != "__":
+        if n[i][0:2] != "__":
             print(n[i])
